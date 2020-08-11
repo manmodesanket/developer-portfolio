4df85a26a32180ad43499db76901d76573db90b4
Sanket Manmode <manmodesanket@gmail.com>
2020-08-11 12:13:26 +0530

    profile improved

--- src/component/Info.js
+++ src/component/Info.js
@@ -5,27 +5,49 @@ const Info = () => {
   const [username, setUsername] = useState("");
   const [bio, setBio] = useState("");
   const [social, setSocial] = useState("");
+  const [location, setLocation] = useState("");
+  const [work, setWork] = useState("");
+  const [looking, setLooking] = useState("");
 
   useEffect(() => {
     setName("John Doe");
     setUsername("johnnyboi");
     setBio("I don't kid anymore. I do things");
     setSocial("");
+    setLocation("Pune, India");
+    setWork("Software Engineer at Amdocs");
+    setLooking("Open for New Oppoertunity");
   });
 
   return (
     <div className="info">
-      <div>
-        <div>{name}</div>
-        <div>@{username}</div>
+      <div className="data">
+        <div>
+          <h1>{name}</h1>
+          <div>@{username}</div>
+          <div>{bio}</div>
+          <ul className="social">
+            <li>LinkedIn</li>
+            <li>Twitter</li>
+            <li>Github</li>
+            <li>Medium</li>
+          </ul>
+        </div>
+        <ul className="status">
+          <li>
+            <div className="key">Work</div>
+            <div className="value">{work}</div>
+          </li>
+          <li>
+            <div className="key">Location</div>
+            <div className="value">{location}</div>
+          </li>
+          <li>
+            <div className="key">Work Status</div>
+            <div className="value">{looking}</div>
+          </li>
+        </ul>
       </div>
-      <div>{bio}</div>
-      <ul className="social">
-        <li>LinkedIn</li>
-        <li>Twitter</li>
-        <li>Github</li>
-        <li>Medium</li>
-      </ul>
     </div>
   );
 };
--- src/component/Profile.js
+++ src/component/Profile.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Projects from "./Projects";
 import Info from "./Info";
+import Skills from "./Skills";
 
 const Profile = () => {
   return (
     <div className="container">
       <Info />
+      <Skills />
       <Projects />
     </div>
   );
