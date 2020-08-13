208bf3bfad33d6590a29de8eaadfb41b4a95fcd6
Sanket Manmode <manmodesanket@gmail.com>
2020-08-13 12:17:43 +0530

    basic projects added

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Projects from "./Projects";
 import Info from "./Info";
 import Skills from "./Skills";
 
 const Profile = () => {
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    setUsername("johndoe");
+    document.title = username;
+  }, [username]);
   return (
     <div className="container">
       <Info />
--- src/component/Projects.js
+++ src/component/Projects.js
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Projects = () => {
-  return <div className="container">Projects</div>;
+  const [projects, setProjects] = useState(null);
+  useEffect(() => {
+    const p = [
+      {
+        name: "Project Title 1",
+        description: "First Project",
+      },
+      {
+        name: "Project Title 2",
+        description: "Second Project",
+      },
+      {
+        name: "Project Title 3",
+        description: "Third Project",
+      },
+    ];
+    setProjects(p);
+  }, []);
+
+  if (projects) {
+    const list = projects.map((p, idx) => {
+      return (
+        <li key={idx}>
+          <div className="card">
+            <div className="card-content">
+              <span className="card-title">{p.name}</span>
+              <div>{p.description}</div>
+            </div>
+          </div>
+        </li>
+      );
+    });
+    return (
+      <div className="container">
+        <ul>{list}</ul>
+      </div>
+    );
+  } else {
+    return <div>Projects</div>;
+  }
 };
 
 export default Projects;
