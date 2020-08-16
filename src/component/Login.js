cb4ea84ddfc9d4aa1baff9beb1e2787dd12f1bdc
Sanket Manmode <manmodesanket@gmail.com>
2020-08-16 19:51:30 +0530

    basic login done

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,19 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import { auth } from "../firebase/firebase";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+
+  const login = () => {
+    auth.signInWithEmailAndPassword(email, pass);
+  };
+  const handleChange = (e) => {
+    if (e.target.name == "email") {
+      setEmail(e.target.value);
+    } else if (e.target.name == "password") {
+      setPass(e.target.value);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("email " + email);
+    console.log("pass  " + pass);
+    login();
+    document.getElementById("login-form").reset();
+  };
+
+  const logout = () => {
+    auth.signOut();
+  };
+
+  if (auth.currentUser) {
+    console.log(auth.currentUser);
+  }
   return (
     <div className="container login-form">
-      <form className="">
+      <form onSubmit={handleSubmit} id="login-form">
         <label>
           Email
-          <input type="text" placeholder="email"></input>
+          <input
+            type="text"
+            name="email"
+            placeholder="email"
+            onChange={handleChange}
+          ></input>
         </label>
         <label>
           Password
-          <input type="password" placeholder="password"></input>
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+            onChange={handleChange}
+          ></input>
         </label>
         <input type="submit" value="Submit" />
       </form>
+      <button onClick={logout}>Logout</button>
     </div>
   );
 };
